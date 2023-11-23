767b57271f7df2eea10b793cfb8b22ed3f55a1c5
Timothy Crundall <tim.crundall@gmail.com>
2023-11-23 11:01:38 +1100

    Add mapping for local grep

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,8 +1,15 @@
 local builtin = require('telescope.builtin')
+local utils = require('telescope.utils')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+
+-- Perform grep using cwd of buffer as root (works within netrw)
+vim.keymap.set('n', '<leader>pl', function()
+  builtin.live_grep({cwd=utils.buffer_dir(),})
+end, {})
 
