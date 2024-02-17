9c6af975095063f3cab2f361168f97a38d11d327
Timothy Crundall <tim.crundall@gmail.com>
2024-02-17 17:30:55 +0100

    Comment out kiwi due to some startup issue
    E5113: Error while calling lua chunk: \
    ...site/pack/packer/start/plenary.nvim/lua/plenary/path.lua:511: \
    FileNotFoundError

--- nvim/after/plugin/kiwi.lua
+++ nvim/after/plugin/kiwi.lua
@@ -1,12 +1,12 @@
-local kiwi = require('kiwi')
+-- local kiwi = require('kiwi')
 
-kiwi.setup({
-  {
-    name = "personal",
-    path = "/Users/tcrundall/Notes"
-  }
-})
+-- kiwi.setup({
+--   {
+--     name = "personal",
+--     path = "/Users/tcrundall/Notes"
+--   }
+-- })
 
--- Necessary keybindings
-vim.keymap.set('n', '<leader>ww', kiwi.open_wiki_index, {})
-vim.keymap.set('n', '<leader>wx', kiwi.todo.toggle, {})
+-- -- Necessary keybindings
+-- vim.keymap.set('n', '<leader>ww', kiwi.open_wiki_index, {})
+-- vim.keymap.set('n', '<leader>wx', kiwi.todo.toggle, {})
