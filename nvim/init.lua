0655a4f87d6d8ea9e4b683e5d5e7ac58d239b2c3
Timothy Crundall <tim.crundall@gmail.com>
2024-03-07 15:09:18 +0100

    Shift plugin details to dedicated module

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,139 +20,15 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-require("lazy").setup({
+require("lazy").setup(
+  "plugins",
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    opts = {
-      dim_inactive_windows = false,
-      styles = {
-        transparancy = true,
-      },
+    change_detection = {
+      -- automatically check for config file changes and reload the ui
+      enabled = true,
+      notify = false, -- get a notification when changes are found
     },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-cmdline",
-    },
-    -- config = function()
-    --   require("cmp").setup({
-    --     sources = {
-    --       { name = "nvim_lsp" },
-    --       { name = "buffer" },
-    --       { name = "path" },
-    --       { name = "luasnip" },
-    --       { name = "nvim_lua" },
-    --     }
-    --   })
-    -- end
-  },
-
-  -- setting up snippets
-  {
-    "L3MON4D3/LuaSnip",
-    -- follow latest release.
-    tag = "v2.0.0", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    -- install jsregexp (optional!:).
-    build = "make install_jsregexp"
-  },
-  { "saadparwaiz1/cmp_luasnip" },
-  { "rafamadriz/friendly-snippets" },
-
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'L3MON4D3/LuaSnip' },
-    }
-  },
-  "nvim-treesitter/nvim-treesitter",
-  -- {
-  -- 	"nvim-treesitter/playground",
-  -- 	{ build = ":TSUpdate" },
-  -- },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.3",
-    dependencies = { { "nvim-lua/plenary.nvim" } }
-  },
-  "mbbill/undotree",
-  "tpope/vim-fugitive",
-  "theprimeagen/harpoon",
-  "nvim-lua/plenary.nvim",
-
-  -- Smooth transitions between tmux and nvim panes
-  "christoomey/vim-tmux-navigator",
-
-  -- File tree
-  {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons', -- optional
-    },
-    config = function()
-      require("nvim-tree").setup({})
-    end
-  },
-
-  -- commenting out stuff (gc<motion>)
-  'tpope/vim-commentary',
-
-  -- Pretty markdown
-  --  {
-  --    "lukas-reineke/headlines.nvim",
-  --    -- "after" is a packer key. move this plugin to
-  --    -- dependencie of nvim-treesitter instead
-  --    after = "nvim-treesitter",
-  --    config = function()
-  --      require("headlines").setup()
-  --    end,
-  --  },
-
-  -- Pretty markdown! Includes link concealment and other hotkeys
-  'ixru/nvim-markdown',
-
-  -- {
-  --   'jakewvincent/mkdnflow.nvim',
-  --   config = function()
-  --     require('mkdnflow').setup({
-  --       -- Config goes here; leave blank for defaults
-  --     })
-  --   end
-  -- },)
-
-  -- -- orgmode
-  -- {
-  --   'nvim-orgmode/orgmode',
-  --   config = function()
-  --     require('orgmode').setup_ts_grammar()
-  --     require('orgmode').setup {}
-  --   end,
-  --   dependencies = {
-  --     { 'hrsh7th/nvim-cmp' },
-  --   }
-  -- },)
-
-  -- vimwiki for nvim
-  {
-    'serenevoid/kiwi.nvim',
-    dependencies = { { 'nvim-lua/plenary.nvim' } }
-  },
-
-  -- for case matching replacement
-  { 'tpope/vim-abolish' },
-})
+  })
 
 -- Set up custom settings and mappings
 require("flower")
