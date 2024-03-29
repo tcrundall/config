50b44a30ee8a9997cc0a82e2fddf5676be485de5
Timothy Crundall <tim.crundall@gmail.com>
2024-03-29 22:46:27 +0100

    Add mapping for go to type definition for C#
    Using my own fork of omnisharp-extended

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,37 +7,19 @@ return { -- LSP Configuration & Plugins
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "Hoffs/omnisharp-extended-lsp.nvim",
+    -- "Hoffs/omnisharp-extended-lsp.nvim",
+    "tcrundall/omnisharp-extended-lsp.nvim",
 
     -- Useful status updates for LSP.
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     { "j-hui/fidget.nvim", opts = {} },
   },
   config = function()
-    -- Brief Aside: **What is LSP?**
-    --
-    -- LSP is an acronym you've probably heard, but might not understand what it is.
-    --
-    -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-    -- and language tooling communicate in a standardized fashion.
-    --
-    -- In general, you have a "server" which is some tool built to understand a particular
-    -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc). These Language Servers
-    -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-    -- processes that communicate with some "client" - in this case, Neovim!
-    --
-    -- LSP provides Neovim with features like:
-    --  - Go to definition
-    --  - Find references
-    --  - Autocompletion
-    --  - Symbol Search
-    --  - and more!
-    --
-    -- Thus, Language Servers are external tools that must be installed separately from
-    -- Neovim. This is where `mason` and related plugins come into play.
-    --
-    -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-    -- and elegantly composed help section, `:help lsp-vs-treesitter`
+    -- For debugging LSP
+    -- vim.lsp.set_log_level("trace")
+    -- if vim.fn.has("nvim-0.5.1") == 1 then
+    --   require("vim.lsp.log").set_format_func(vim.inspect)
+    -- end
 
     --  This function gets run when an LSP attaches to a particular buffer.
     --    That is to say, every time a new file is opened that is associated with
@@ -64,45 +46,15 @@ return { -- LSP Configuration & Plugins
           vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
         end
 
-        -- Jump to the definition of the word under your cursor.
-        --  This is where a variable was first declared, or where a function is defined, etc.
-        --  To jump back, press <C-T>.
         map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-        -- Find references for the word under your cursor.
         map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-        -- Jump to the implementation of the word under your cursor.
-        --  Useful when your language has ways of declaring types without an actual implementation.
         map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
-        -- Jump to the type of the word under your cursor.
-        --  Useful when you're not sure what type a variable is and you want to see
-        --  the definition of its *type*, not where it was *defined*.
         map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-
-        -- Fuzzy find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
         map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-
-        -- Fuzzy find all the symbols in your current workspace
-        --  Similar to document symbols, except searches over your whole project.
         map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-        -- Rename the variable under your cursor
-        --  Most Language Servers support renaming across files, etc.
         map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-
-        -- Execute a code action, usually your cursor needs to be on top of an error
-        -- or a suggestion from your LSP for this to activate.
         map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-        -- Opens a popup that displays documentation about the word under your cursor
-        --  See `:help K` for why this keymap
         map("K", vim.lsp.buf.hover, "Hover Documentation")
-
-        -- WARN: This is not Goto Definition, this is Goto Declaration.
-        --  For example, in C this would take you to the header
         map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
         if client.name == "omnisharp" then
@@ -110,6 +62,9 @@ return { -- LSP Configuration & Plugins
           map("gd", require("omnisharp_extended").lsp_definition, "Ext. [G]oto [D]efinition")
           map("gr", require("omnisharp_extended").lsp_references, "Ext. [G]oto [R]eferences")
           map("gI", require("omnisharp_extended").lsp_implementation, "Ext. [G]oto [I]mplementation")
+
+          -- This is my own function from my fork of omnisharp_extended
+          map("<leader>D", require("omnisharp_extended").lsp_type_definition, "Ext. [G]oto [D]efinition")
         end
 
         -- Omnisharp doesn't always handle decompiled files well, so we don't allow it here
