8d4ea84e279c3254070efacb1e436a92ae5421d2
Timothy Crundall <tim.crundall@gmail.com>
2024-04-01 20:38:02 +0200

    Extend link creator to choose either branch or commit

--- nvim/lua/custom/user-functions.lua
+++ nvim/lua/custom/user-functions.lua
@@ -6,14 +6,22 @@
 local myfunc = function(opts)
   local base_url = "https://github.com/tcrundall/"
   local repo_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-  -- local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
-  local commit = vim.fn.system("git log -n 1 --pretty='format:%H'")
+
+  local target
+  if opts.args == "commit" then
+    -- Get perma link to commit
+    target = vim.fn.system("git log -n 1 --pretty='format:%H'")
+  else
+    -- Link to branch
+    target = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+  end
+
   local filename = vim.fn.expand("%:p:.")
-  local url = base_url .. repo_name .. "/blob/" .. commit .. "/" .. filename .. "#L" .. opts.line1 .. "-L" .. opts.line2
+  local url = base_url .. repo_name .. "/blob/" .. target .. "/" .. filename .. "#L" .. opts.line1 .. "-L" .. opts.line2
   vim.fn.setreg("*", url)
   return url
 end
 
 vim.api.nvim_create_user_command("LinkToCode", function(opts)
   myfunc(opts)
-end, { range = true })
+end, { range = true, nargs = "?" })
