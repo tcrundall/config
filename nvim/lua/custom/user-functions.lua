4928a55c7b310d259868a592abc75d696f902888
Timothy Crundall <timothy.crundall@thermofisher.com>
2024-04-02 19:26:45 +0200

    Add another link method

--- nvim/lua/custom/user-functions.lua
+++ nvim/lua/custom/user-functions.lua
@@ -3,8 +3,8 @@
 --   * directory name == repository name
 --   * hosted on my personal github
 --   * we want a permanent link ==> use a commit hash
-local myfunc = function(opts)
-  local base_url = "https://github.com/tcrundall/"
+local githubLink = function(opts)
+  local base_url = "https://github.com/tcrundall"
   local repo_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
 
   local target
@@ -19,9 +19,45 @@ local myfunc = function(opts)
   local filename = vim.fn.expand("%:p:.")
   local url = base_url .. repo_name .. "/blob/" .. target .. "/" .. filename .. "#L" .. opts.line1 .. "-L" .. opts.line2
   vim.fn.setreg("*", url)
+  print(url)
   return url
 end
 
-vim.api.nvim_create_user_command("LinkToCode", function(opts)
-  myfunc(opts)
+local adoLink = function(opts)
+  local base_url = os.getenv("DEFAULT_REPO_URL")
+  local repo_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+
+  local target
+  if opts.args == "commit" then
+    -- Get perma link to commit
+    target = "GC" .. vim.fn.system("git log -n 1 --pretty='format:%H'")
+  else
+    -- Link to branch
+    target = "GB" .. vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+  end
+
+  local filename = vim.fn.expand("%:p:.")
+  local url = base_url
+    .. repo_name
+    .. "?path=/"
+    .. filename
+    .. "&version="
+    .. target
+    .. "&line="
+    .. opts.line1
+    .. "&lineEnd="
+    .. opts.line2
+    .. "&lineStartColumn=0&lineEndColumn=1000"
+  vim.fn.setreg("*", url)
+  vim.fn.setreg("+", url)
+  print(url)
+  return url
+end
+
+vim.api.nvim_create_user_command("LinkToGit", function(opts)
+  githubLink(opts)
+end, { range = true, nargs = "?" })
+
+vim.api.nvim_create_user_command("LinkToAdo", function(opts)
+  adoLink(opts)
 end, { range = true, nargs = "?" })
