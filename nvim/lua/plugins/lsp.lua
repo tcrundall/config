1dd2e1e73f61de139c01e33b4dd8a4ab860a294d
Timothy Crundall <tim.crundall@gmail.com>
2024-04-07 23:13:54 +0200

    Configure convenince maps for Zig

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -112,6 +112,7 @@ return { -- LSP Configuration & Plugins
       -- clangd = {},
       gopls = {},
       pyright = {},
+      zls = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
