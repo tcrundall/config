633ea10098302b2c7997cfad6d7b9d95ae95c925
Timothy Crundall <tim.crundall@gmail.com>
2024-05-11 22:21:18 +0200

    Set up easier dark/light theme switching

--- nvim/lua/custom/colors.lua
+++ nvim/lua/custom/colors.lua
@@ -18,60 +18,86 @@ See default nvim groups with `:help highlight-groups`
 See nvim tree groups with: `:NvimTreeHiTest`
 
 --]]
+
+local highlight_groups = {
+  -- Inferred:
+  -- Needed for rose-pine but not for tokyonight
+  -- Changing this also changes color of fugitive text
+  -- 'Normal',
+  -- 'NormalNC',
+
+  -- 'NormalFloat',
+  -- 'NormalSB', -- Side Bars, e.g. help in TokyoNight
+
+  -- 'Inactive',
+  "SignColumnSB",
+  "MsgArea",
+  -- 'WinBar',
+  "WinSeparator",
+  -- 'All',
+
+  -- Telescope
+  "TelescopeNormal",
+  "TelescopeBorder",
+
+  -- which key
+  -- 'WhichKeyFloat',
+
+  -- nvim tree
+  "NvimTreeNormal",
+  "NvimTreeNormalNC",
+  "NvimTreeStatusLine",
+  "NvimTreeStatusLineNC",
+  -- 'NvimTreeWinSeparator',
+}
+
 function SetBackground()
-  local highlight_groups = {
-    -- Inferred:
-    -- Needed for rose-pine but not for tokyonight
-    -- Changing this also changes color of fugitive text
-    -- 'Normal',
-    -- 'NormalNC',
-
-    -- 'NormalFloat',
-    -- 'NormalSB', -- Side Bars, e.g. help in TokyoNight
-
-    -- 'Inactive',
-    "SignColumnSB",
-    "MsgArea",
-    -- 'WinBar',
-    "WinSeparator",
-    -- 'All',
-
-    -- Telescope
-    "TelescopeNormal",
-    "TelescopeBorder",
-
-    -- which key
-    -- 'WhichKeyFloat',
-
-    -- nvim tree
-    "NvimTreeNormal",
-    "NvimTreeNormalNC",
-    "NvimTreeStatusLine",
-    "NvimTreeStatusLineNC",
-    -- 'NvimTreeWinSeparator',
-  }
   for _, hl in pairs(highlight_groups) do
     -- vim.api.nvim_set_hl(0, hl, { bg = 'none', fg = 'none' })
     vim.api.nvim_set_hl(0, hl, { bg = "none" })
   end
 end
 
-function DarkTheme()
-  vim.cmd.colorscheme("tokyonight-night")
-  -- vim.cmd.colorscheme 'rose-pine-main'
-  SetBackground()
+function UnsetBackground()
+  for _, hl in pairs(highlight_groups) do
+    vim.api.nvim_set_hl(0, hl, { bg = 1 })
+  end
 end
+
+function DarkTheme()
+  require("tokyonight").setup({ transparent = true })
+  vim.cmd.colorscheme("tokyonight-night")
+  -- SetBackground()
+end
+
 function DarkThemeOpaque()
-  -- vim.cmd.colorscheme 'tokyonight-night'
-  vim.cmd.colorscheme("rose-pine-main")
+  require("tokyonight").setup({ transparent = false })
+  vim.cmd.colorscheme("tokyonight-night")
+  -- vim.cmd.colorscheme("rose-pine-main")
 end
 
 function LightTheme()
-  vim.cmd("colorscheme tokyonight-day")
+  -- vim.cmd("colorscheme tokyonight-day")
+  require("catppuccin").setup({ transparent_background = true })
+  vim.cmd("colorscheme catppuccin-latte")
+  -- UnsetBackground()
+end
+
+function LightThemeOpaque()
+  -- vim.cmd("colorscheme tokyonight-day")
+  require("catppuccin").setup({ transparent_background = false })
+  vim.cmd("colorscheme catppuccin-latte")
+  -- UnsetBackground()
 end
 
 -- [TH]eme [D]ark
 vim.keymap.set("n", "<leader>thd", DarkTheme)
 
+-- [TH]eme [O]paque [D]ark
+vim.keymap.set("n", "<leader>thod", DarkThemeOpaque)
+
 -- [TH]eme [L]ight
 vim.keymap.set("n", "<leader>thl", LightTheme)
+
+-- [TH]eme [O]paque [L]ight
+vim.keymap.set("n", "<leader>thol", LightThemeOpaque)
