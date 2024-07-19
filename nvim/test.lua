a5a87bd13405c86b297f18ab5b173df8709547f5
Timothy Crundall <tim.crundall@gmail.com>
2024-07-19 19:19:51 +0200

    TEMP Refactor step 1

--- nvim/test.lua
+++ nvim/test.lua
@@ -1,11 +1,7 @@
-local function follow_link()
-  local current_line = vim.api.nvim_get_current_line()
-  local cursor_pos = vim.api.nvim_win_get_cursor(0)
-  local search_start = cursor_pos[2]
+local function extract_link_from_string(str, search_start)
   local open_paren_ix, close_paren_ix = nil, nil
-
   while true do
-    close_paren_ix = string.find(current_line, "%)", search_start)
+    close_paren_ix = string.find(str, "%)", search_start)
     print(close_paren_ix)
     if close_paren_ix == nil then
       print("Could not find ')' after cursor position")
@@ -13,7 +9,7 @@ local function follow_link()
     else
       -- print("Found ) at " .. close_paren_ix)
     end
-    open_paren_ix = FindLast(current_line, "%(", close_paren_ix)
+    open_paren_ix = FindLast(str, "%(", close_paren_ix)
 
     if open_paren_ix == nil then
       print("Could not find matching '(' before column " .. close_paren_ix)
@@ -22,10 +18,10 @@ local function follow_link()
 
     -- Check no closing parentheses inside
     local extra_close_paren_ix =
-      string.find(string.sub(current_line, open_paren_ix, close_paren_ix - 1), "%)")
+      string.find(string.sub(str, open_paren_ix, close_paren_ix - 1), "%)")
     print("extra_close_paren_ix: ", extra_close_paren_ix)
     -- Check open parenthesis preceded by square bracket
-    local leading_char = string.sub(current_line, open_paren_ix - 1, open_paren_ix - 1)
+    local leading_char = string.sub(str, open_paren_ix - 1, open_paren_ix - 1)
     if extra_close_paren_ix == nil and leading_char == "]" then
       break
     else
@@ -33,7 +29,17 @@ local function follow_link()
       print("Searching again from: ", search_start)
     end
   end
-  local address = string.sub(current_line, open_paren_ix + 1, close_paren_ix - 1)
+  return string.sub(str, open_paren_ix + 1, close_paren_ix - 1)
+end
+
+local function follow_link()
+  local current_line = vim.api.nvim_get_current_line()
+  local search_start = vim.api.nvim_win_get_cursor(0)[2]
+
+  local address = extract_link_from_string(current_line, search_start)
+  if address == nil then
+    return
+  end
   -- print("Address: ", address)
 
   if string.sub(address, 1, 1) == "#" then
