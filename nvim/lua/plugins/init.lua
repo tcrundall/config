f8b5d79e0edf3093fbbc622ba758e9ce7b82739d
Timothy Crundall <tim.crundall@gmail.com>
2025-05-30 23:03:01 +0200

    Stay in nvim when zoomed and switching panes

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -17,7 +17,21 @@ return {
   },
 
   "tpope/vim-abolish", -- handle variations of words in e.g. substituion
-  "christoomey/vim-tmux-navigator", -- switch between tmux and nvim panes
+
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = true,
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+    config = function()
+      vim.g.tmux_navigator_disable_when_zoomed = 1
+    end,
+  },
   {
     "nvim-lua/plenary.nvim",
     config = function()
